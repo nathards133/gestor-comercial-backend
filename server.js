ae3150e1a28247a4497fcd868f5ce15d2435f7b6
nathafaculdade <nathafaculdade@gmail.com>
2024-09-22 20:25:42 -0300

    agora vai

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const compression = require('compression');
 const auth = require('./middleware/auth');
 
 const app = express();
