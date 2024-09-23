edc5c9fe222ad01d37f92206cfca428bedf42b49
nathafaculdade <nathafaculdade@gmail.com>
2024-09-23 13:07:38 -0300

    fixes

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'https://gestor-comercial-panel-67e8m1qn1-nathards133s-projects.vercel.app',
     'gestor-comercial-backend.vercel.app',
     'http://localhost:3000'
 ],
